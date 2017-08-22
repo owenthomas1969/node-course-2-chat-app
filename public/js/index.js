83ea55878ad5f22dcd8f0244176ba375463dd889
Owen Thomas <opthomas@xtra.co.nz>
2017-08-23 10:16:19 +1200

    Add form for message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,21 @@ socket.on('disconnect', function() {
 //     console.log('New Email', email);
 // });
 
-socket.on('newMessage', function(email) {
-    console.log('New chat message', email);
+socket.on('newMessage', function(message) {
+    console.log('New chat message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User', 
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -22,10 +22,13 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage',  generateMessage('Admin', 'New User joined chat rooom'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('New message ', message);
+        
         io.emit('newMessage',  generateMessage(message.from, message.text));
-        });
+        callback('This is from the server');
+    });
+
         // socket.broadcast.emit('newMessage', {
         //         from: message.from,
         //         text: message.text,
