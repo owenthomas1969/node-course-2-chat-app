20e7d182c39585701be56c59b29904460d3ba41a
Owen Thomas <opthomas@xtra.co.nz>
2017-08-23 09:45:41 +1200

    Generate Message utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -17,14 +18,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.emit('newMessage',  generateMessage('Admin', 'New User joined chat rooom'));
+
     socket.on('createMessage', (message) => {
         console.log('New message ', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime
+        io.emit('newMessage',  generateMessage(message.from, message.text));
         });
-    });
+        // socket.broadcast.emit('newMessage', {
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime
+        //     });
+        //});
 
     socket.on('disconnect', () => {
         console.log('User disconnected from server');
