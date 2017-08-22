77713ee7ff5db3959afca4533bfab8f73037c7bd
Owen Thomas <opthomas@xtra.co.nz>
2017-08-23 09:25:36 +1200

    Emit newMessage

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+// socket.on('newEmail', function(email) {
+//     console.log('New Email', email);
+// });
+
+socket.on('newMessage', function(email) {
+    console.log('New chat message', email);
+});
